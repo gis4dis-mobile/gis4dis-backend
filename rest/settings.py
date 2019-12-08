26c3a21c67233091d8564ff4a05f2e38175fa784
Petr Šilhák <petrsilhak@gmail.com>
2019-12-08 14:56:42 +0100

    Page size changed

--- rest/settings.py
+++ rest/settings.py
@@ -86,7 +86,7 @@ REST_FRAMEWORK = {
         #'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 200
+    'PAGE_SIZE': 500
 }
 
 REST_AUTH_SERIALIZERS = {
